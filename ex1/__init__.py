4103a705ceded39e191d558eef06cb3819176bb6
Rivan <rivan_git@mail.ru>
2023-10-04 17:59:52 +0300

    Решение Exercise 1

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -1,7 +1,27 @@
-def build_roles_tree(mapping):
+from ex1.mapping import RAW_MAPPING
+
+
+def build_roles_tree(mapping_data):
     """
-    :param mapping: маппинг ролей в категории
+    :param mapping_data: маппинг ролей в категории
     :return: дерево ролей
     """
-    # put your code here
-    pass
+
+    categories = mapping_data.get('categories')
+    roles = mapping_data.get('roles')
+
+    for role in roles.values():
+        role['text'] = role.pop('name')
+
+    sorted_list = mapping_data.get('categoryIdsSorted')
+    full_categories = {'categories': []}
+
+    for category_id in sorted_list:
+        full_categories['categories'].append(categories.get(category_id))
+
+    for cat in full_categories['categories']:
+        cat['id'] = f'category-{cat["id"]}'
+        cat['text'] = cat.pop('name')
+        cat['items'] = [roles[i] for i in cat.pop('roleIds')]
+
+    return full_categories
